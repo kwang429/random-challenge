cc72c6a1c37bc96f68e037d0cdce3e31bf872382
Kat <katherine.wang429@gmail.com>
2020-03-19 12:44:46 -0700

    Add schema.sql file and upload

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+/*
+  Database must exist first, before uploading this file
+  After database creation, use command: psql [db name] < schema.sql
+*/
+
+DROP TABLE IF EXISTS challenges CASCADE;
+DROP TABLE IF EXISTS completed CASCADE;
+
+CREATE TABLE challenges(
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  category TEXT [],
+  link TEXT,
+  completed Boolean
+);
+
+CREATE TABLE completed(
+  id SERIAL PRIMARY KEY,
+  ref_id INTEGER REFERENCES challenges(id)
+);
