f46710f21d6c65ff698bf74e05633199147aed7e
Kat <katherine.wang429@gmail.com>
2020-03-19 15:36:34 -0700

    Modify db func names

--- db/dbHelpers.js
+++ db/dbHelpers.js
@@ -1,7 +1,7 @@
 const db = require('./index.js');
 
 const dbHelpers = {
-  add: function(data, callback) {
+  addChallenge: function(data, callback) {
     let queryString = `INSERT INTO challenges(name, category, link) VALUES (${data.name}, ${data.category}, ${data.link})`;
     db.query(queryString, (err, result) => {
       if (err) {
@@ -11,7 +11,7 @@ const dbHelpers = {
       }
     });
   },
-  delete: function(id, callback) {
+  deleteChallenge: function(id, callback) {
     let queryString = `DELETE FROM challenges WHERE id=${id}`;
     db.query(queryString, (err, result) => {
       if (err) {
@@ -21,7 +21,7 @@ const dbHelpers = {
       }
     });
   },
-  done: function(id, callback) {
+  completeChallenge: function(id, callback) {
     let updateStr = `UPDATE challenges SET completed = true WHERE id = ${id}`;
     let addStr = `INSERT INTO completed (ref_id) VALUES (${id})`;
     db.query(updateStr, (err, result) => {
