01a2256c7b600dc95f6b1dd2c58d6446ac2a3d8d
Kat <katherine.wang429@gmail.com>
2020-03-25 16:29:50 -0700

    Try out react hooks on text input

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -9,17 +9,24 @@ function Form() {
     <div>
       <div>
         <b>Challenge Name: </b>
-        <input></input>
+        <input onChange={e => setName(e.target.value)}></input>
       </div>
       <div>
         <b>Challenge Link: </b>
-        <input></input>
+        <input onChange={e => setLink(e.target.value)}></input>
       </div>
       <div>
         <b>Challenge Category(s): </b>
         <select>
           <option></option>
         </select>
+      </div>
+      <div>
+        <button
+          onClick={() => console.log(`You're submitting ${name} and ${link}`)}
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
