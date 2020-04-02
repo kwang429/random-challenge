404d81798f8d55728ea54d55acc011b80eea48d3
Kat <katherine.wang429@gmail.com>
2020-04-02 12:11:55 -0700

    useEffect no longer starts an infinite loop

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Form from './Form.jsx';
+import axios from 'axios';
 import styled from 'styled-components';
 
 function App() {
+  const [allCats, setCats] = useState([]);
+
+  const updateCategories = function() {
+    axios.get('/categories').then(({ data }) => {
+      setCats(allCats.concat(data));
+    });
+  };
+
+  useEffect(() => {
+    updateCategories();
+  }, []);
+
   return (
     <div>
-      <Form />
+      <Form categories={allCats} />
     </div>
   );
 }
